9fba262e5eddb3d052bba742a356780753ead434
Maksim Korobov <korobov_m04@mail.ru>
2024-01-06 22:11:11 +0300

    added log functions

--- develop/server/middleware/middleware.go
+++ develop/server/middleware/middleware.go
@@ -1,6 +1,9 @@
 package middleware
 
 import (
+	"encoding/json"
+	"log"
+	"net/http"
 	time2 "time"
 
 	"github.com/mkorobovv/L2/develop/server/models"
@@ -15,4 +18,21 @@ func NewEvent(des, date, time string, userId int) *models.Event {
 		Description: des,
 		Time:        time,
 	}
+}
+
+func ErrorLogger(w http.ResponseWriter, errorInput error) {
+	log.Println(errorInput)
+	w.WriteHeader(http.StatusInternalServerError)
+	details := models.Details{ErrCode: http.StatusInternalServerError, ErrMessage: errorInput.Error()}
+	response, _ := json.MarshalIndent(models.Error{Err: details}, "", "\t")
+	w.Write(response)
+	return
+}
+
+func ResponseLogger(w http.ResponseWriter, message string) {
+	w.WriteHeader(http.StatusInternalServerError)
+	result := models.Result{StatusCode: http.StatusOK, Message: message}
+	response, _ := json.MarshalIndent(models.Response{Body: result}, "", "\t")
+	w.Write(response)
+	return
 }
